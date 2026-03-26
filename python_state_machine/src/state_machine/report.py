6e44e0d338feaab3c64b4fd55578683bebff0c8a
Nuria Malet Quintar <maletq.pr@gmail.com>
2026-03-26 20:19:10 -0300

    report mvp1 completed

--- python_state_machine/src/state_machine/report.py
+++ python_state_machine/src/state_machine/report.py
@@ -1,52 +1,97 @@
 import logging
-import json
 import os, sys
 from config import *
+from pydantic import BaseModel, RootModel
+from typing import List,Optional
+from pathlib import Path
 
 
-lines_to_write = [
-    "# Scenario Report\n\n",
-    "## Scenario\n",
-    "- **ID:** SCN-001\n",
-    "- **Number of steps:** 18\n",
-    "- **Final state:** EntropyComplete\n",
-    "- **Feature completed:** NO\n\n",
-    "## Events\n",
-    "1- ClarifySomehow\n",
-    "2- StartAnyway\n",
-    "3- DiscoverDisagreement\n",
-    "4- Postpone\n",
-    "5- ForgetForLongTime\n",
-    "6- ForgetForLongTime\n",
-    "7- CustomerComplains\n",
-    "8- ExecutiveRemembers\n",
-    "9- DiscoverDisagreement\n",
-    "10- StartAnyway\n",
-    "11- SendToQA\n",
-    "12- Rework\n",
-    "13- SendToQA\n",
-    "14- RejectFundamentally\n",
-    "15- Postpone\n",
-    "16- ForgetForLongTime\n",
-    "17- AuditDiscovers\n",
-    "18- DeclareEntropyComplete\n\n",
-    "## Warnings\n",
-    "The feature was abandoned.\n\n",
-   " ## Summary\n",
-    "This scenario ended in `EntropyComplete`. It was abandoned after several attempts, disagreement among developers in how to proceed, and QA rejection.\n\n"
-]
+class Error(BaseModel):
+    step: Optional[int] = None
+    event: Optional[str] = None
+    phase: Optional[str] = None
+    message: str
 
-def create_report(args):
+class Context(BaseModel):
+    revival_signals: int
+    qa_rejections: int
+    sprints_ignored: int
+    
+class Step(BaseModel):
+    step: int
+    event: str
+    resulting_state: str
+    resulting_context: Context    
+    
+class State(BaseModel):
+    scenario_id: str
+    status: str
+    error: Optional[Error] = None
+    last_state: Optional[str] = None
+    last_context: Optional[Context] = None
+    final_state: Optional[str] = None
+    final_context: Optional[Context] = None
+    steps: Optional[List[Step]] = None
+  
+
+def create_md(state_instance: State)-> List[str]:
+    lines_to_write = [
+        "# Scenario Report\n\n",
+        "## Scenario\n",
+        f"- **ID:** {state_instance.scenario_id}\n",
+        f"- **Status:** {state_instance.status}\n",
+    ]
+    if state_instance.error is not None:
+        lines_to_write.append(f"- **Error:**\n")
+        if state_instance.error.step is not None:
+            lines_to_write.append(f"    - step: {state_instance.error.step}\n")
+            lines_to_write.append(f"    - event: {state_instance.error.event}\n")
+            lines_to_write.append(f"    - message: {state_instance.error.message}\n")
+            lines_to_write.append(f"- **Last state:** {state_instance.last_state}\n") 
+            lines_to_write.append(f"- **Last context:**\n")  
+            lines_to_write.append(f"    - revival_signals: {state_instance.last_context.revival_signals}\n")
+            lines_to_write.append(f"    - qa_rejections: {state_instance.last_context.qa_rejections}\n")
+            lines_to_write.append(f"    - sprints_ignored: {state_instance.last_context.sprints_ignored}\n\n")
+        else:    
+            lines_to_write.append(f"    - phase: {state_instance.error.phase}\n")
+            lines_to_write.append(f"    - message: {state_instance.error.message}\n")
+    else:
+        lines_to_write.append(f"- **Final state:** {state_instance.final_state}\n") 
+        lines_to_write.append(f"- **Final context:**\n")   
+        lines_to_write.append(f"    - revival_signals: {state_instance.final_context.revival_signals}\n")
+        lines_to_write.append(f"    - qa_rejections: {state_instance.final_context.qa_rejections}\n")
+        lines_to_write.append(f"    - sprints_ignored: {state_instance.final_context.sprints_ignored}\n\n")
+    if state_instance.steps is not None:
+        lines_to_write.append("## Events\n")
+        for s in state_instance.steps:
+            lines_to_write.append(f"{s.step}. {s.event}\n")
+            lines_to_write.append(f"    - resulting_state: {s.resulting_state} \n")
+            lines_to_write.append(f"    - context:\n")
+            lines_to_write.append(f"        - revival_signals: {s.resulting_context.revival_signals}\n")
+            lines_to_write.append(f"        - qa_rejections: {s.resulting_context.qa_rejections}\n")
+            lines_to_write.append(f"        - sprints_ignored: {s.resulting_context.sprints_ignored}\n")
+    return lines_to_write
+
+def store_report(args, lines_to_write: List[str]):
     arg = get_args(args)
     if arg is not None:
         file = os.path.join(set_output_path(), arg+'.md')
     else:    
         file = os.path.join(set_output_path(), get_file_name_report())
-    try:    
+    try:   
+        file = './files/reports/report3.md' 
         with open(file, "w") as f:
             f.writelines(lines_to_write)
             print(f'File {file} created.')
     except Exception as e:
         logging.error(f"Failed to write file {file}: {e}") 
         sys.exit(1)    
+    
+def create_report(args):
+    json_data = Path("./files/data/output3.json").read_text()
+    instance = State.model_validate_json(json_data)
+    lines = create_md(instance) 
+    store_report(args, lines)
+    
+    
     
