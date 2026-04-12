366d2cb66b3e573b5d46ebb0023c899196fb4570
Nuria Malet Quintar <maletq.pr@gmail.com>
2026-04-12 18:02:37 -0300

    improved logs

--- python_state_machine/src/state_machine/cli/scenarios.py
+++ python_state_machine/src/state_machine/cli/scenarios.py
@@ -22,13 +22,13 @@ def get_output_file_from_args(args: argparse.Namespace) -> str:
     
 def read_scenario(args):
     in_file = get_input_file_from_args(args)
-    logger.info(f"inputFile: {in_file}")
+    logger.info(f"input file name: {in_file}")
     sm_data = get_data_full_path()
     if in_file is not None:
         file = os.path.join(sm_data, in_file)
     else:
         file = os.path.join(sm_data, 'scenario.json')    
-    logger.info(f"file: {file}")
+    logger.info(f"full input file: {file}")
     try:
         with open(file, 'r', encoding='utf-8') as file:
             data = json.load(file)
@@ -40,15 +40,14 @@ def read_scenario(args):
     
 def create_scenarios(args):
     out_file = get_output_file_from_args(args)
-    logger.info(f"outputFile: {out_file}")
+    logger.info(f"output file name: {out_file}")
     if out_file is not None:
         file = os.path.join(css.set_path('scenarios'), out_file)
     else:    
         file = os.path.join(css.set_path('scenarios'), get_file_name_scenario())
     payload = read_scenario(args) 
     if payload is not None:  
-        logger.info(f"file: {file}")
-        logger.info(f"payload: {payload}")
+        logger.info(f"full output file: {file}")
         css.create_scenarios(payload,file)
     else:
         logger.error("Payload cannot be null")    
