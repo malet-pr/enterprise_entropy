e381a94a32237ad561ea4c0d91a0bce50755cb5d
Nuria Malet Quintar <maletq.pr@gmail.com>
2026-04-29 19:00:04 -0300

    some cleanup

--- ocaml_rules/bin/main.ml
+++ ocaml_rules/bin/main.ml
@@ -39,24 +39,3 @@ let () =
   | Yojson.Safe.Util.Type_error (msg, _) -> print_endline ("Type error: " ^ msg)
 
 
-(* let () = 
-  let meeting1 = make_meeting Daily 15 in 
-  let meeting2 = make_meeting (CollectiveDebuggingInEnvironment Testing) 45 in
-  let participants = [ 
-    {(make_participant SM) with interested = false}; 
-    {(make_participant ActingLead) with interested = true; understands = true}; 
-    {(make_participant Developer) with understands = true}; 
-    (make_participant Developer); 
-    (make_participant Developer); 
-    {(make_participant DataEngineer) with interested = true; understands = true}; 
-  ] in 
-  let issue1 = {(make_issue High) with understood_by = [Technical] } in 
-  let issue2 = (make_issue Medium) in 
-  let state1 = { meeting = meeting1; participants = participants; issue = issue1; fired_rules = [] ; } in 
-  let state2 = { meeting = meeting2; participants = participants; issue = issue2; fired_rules = [] ; } in 
-  let final_state1 = run_multiple_rules state1 all_rules in 
-  print_final_state(final_state1);
-  let final_state2 = run_multiple_rules state2 all_rules in 
-  print_final_state(final_state2);
- *)
-
